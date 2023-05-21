160ae484ea4c7ca1fadd225cdba2ae81926b5f5e
kwangure <kaf.wangure@gmail.com>
2023-05-21 20:13:07 +0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	throw Error('Show me my stack trace 😭. Errors with a lot of details are abbreviated and cut out but I want to see the full thing.');
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
